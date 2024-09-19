ce11d07f4d940d9ee67411f1cb1df2fe258ca6c2
Christopher <129872252+damourChris@users.noreply.github.com>
2024-09-19 12:19:50 +0000

    docs: add Changelog package as a dependency in Project.toml
    generate links to commits issues etc

--- docs/make.jl
+++ docs/make.jl
@@ -1,23 +1,31 @@
 using GNNCellTypeReferenceGraph
 using Documenter
 
-DocMeta.setdocmeta!(GNNCellTypeReferenceGraph, :DocTestSetup, :(using GNNCellTypeReferenceGraph); recursive=true)
+using Changelog
+
+DocMeta.setdocmeta!(GNNCellTypeReferenceGraph, :DocTestSetup,
+                    :(using GNNCellTypeReferenceGraph); recursive=true)
+
+Changelog.generate(Changelog.CommonMark(),
+                   "CHANGELOG.md";
+                   repo="damourChris/GNNCellTypeReferenceGraph.jl")
+
+Changelog.generate(Changelog.Documenter(),                 # output type
+                   joinpath(@__DIR__, "../CHANGELOG.md"),  # input file
+                   joinpath(@__DIR__, "src/CHANGELOG.md"); # output file
+                   repo="damourChris/GNNCellTypeReferenceGraph.jl",)
 
 makedocs(;
-    modules=[GNNCellTypeReferenceGraph],
-    authors="Chris Damour",
-    sitename="GNNCellTypeReferenceGraph.jl",
-    format=Documenter.HTML(;
-        canonical="https://damourChris.github.io/GNNCellTypeReferenceGraph.jl",
-        edit_link="main",
-        assets=String[],
-    ),
-    pages=[
-        "Home" => "index.md",
-    ],
-)
+         modules=[GNNCellTypeReferenceGraph],
+         authors="Chris Damour",
+         sitename="GNNCellTypeReferenceGraph.jl",
+         format=Documenter.HTML(;
+                                canonical="https://damourChris.github.io/GNNCellTypeReferenceGraph.jl",
+                                edit_link="main",
+                                assets=String[],),
+         pages=["Home" => "index.md",
+                "Changelog" => "CHANGELOG.md"],)
 
 deploydocs(;
-    repo="github.com/damourChris/GNNCellTypeReferenceGraph.jl",
-    devbranch="main",
-)
+           repo="github.com/damourChris/GNNCellTypeReferenceGraph.jl",
+           devbranch="main",)

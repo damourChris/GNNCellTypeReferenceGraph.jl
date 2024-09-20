1ab7c8a0cb4f41b70ec0293443d1883df6cdb373
Chris Damour <129872252+damourChris@users.noreply.github.com>
2024-09-20 23:48:02 +0000

    feat: add sanbox related files and data

---
+++ sandbox/sandbox.jl
@@ -0,0 +1,40 @@
+using Pkg
+Pkg.activate(@__DIR__)
+
+using ExpressionData
+using EnsemblGeneMapping
+using OntologyMapping
+
+using GNNCellTypeReferenceGraph
+
+eset = load_eset(joinpath(@__DIR__, "data", "GSE22886-GPL96_series_matrix.rds"))
+mapped_eset = map_to_ensembl(eset, "entrezgene_id"; gene_col="ENTREZ_GENE_ID")
+
+replacements = ("cells" => "cell",
+                "Naïve" => "naive",
+                "Monocytes" => "monocyte",
+                "Macrophages" => "macrophage",
+                "Neutrophils" => "neutrophil",
+                "CD8+" => "CD8",
+                "CD4+" => "CD4",
+                "Tregs" => "Treg")
+
+ontology_mapped_eset = map_to_ontology(mapped_eset, "cl"; ontology_col="cell type:ch1",
+                                       replacements=replacements)
+
+# Now we need to get the pairings
+# -> This step is to establish the links between genes and cell types
+# Here, lets just randomly assign some genes to cell types
+# while making sure that no gene is assigned to more than one cell type
+pairings = Dict{Symbol,Vector{String}}()
+for cell_type in unique(phenotype_data(ontology_mapped_eset)[!, "cell type:ch1"])
+    # generate N random genes
+    N = rand(15:50)
+    genes = sample(feature_names(ontology_mapped_eset), N; replace=false)
+    pairings[Symbol(cell_type)] = genes
+end
+
+using OntologyTrees
+
+# Create the reference graph
+onto_tree = create_reference_graph(pairings)

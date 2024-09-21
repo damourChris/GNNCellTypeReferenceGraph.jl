f54e2171b6cbde0faa57be668eaf528a6f5f7df4
Chris Damour <129872252+damourChris@users.noreply.github.com>
2024-09-21 00:32:14 +0000

    fix: wrong col name for ontology ids refactor: import the module instead of "using " it

--- sandbox/sandbox.jl
+++ sandbox/sandbox.jl
@@ -4,8 +4,10 @@ Pkg.activate(@__DIR__)
 using ExpressionData
 using EnsemblGeneMapping
 using OntologyMapping
+using OntologyTrees
+using StatsBase
 
-using GNNCellTypeReferenceGraph
+include("../src/GNNCellTypeReferenceGraph.jl")
 
 eset = load_eset(joinpath(@__DIR__, "data", "GSE22886-GPL96_series_matrix.rds"))
 mapped_eset = map_to_ensembl(eset, "entrezgene_id"; gene_col="ENTREZ_GENE_ID")
@@ -27,14 +29,14 @@ ontology_mapped_eset = map_to_ontology(mapped_eset, "cl"; ontology_col="cell typ
 # Here, lets just randomly assign some genes to cell types
 # while making sure that no gene is assigned to more than one cell type
 pairings = Dict{Symbol,Vector{String}}()
-for cell_type in unique(phenotype_data(ontology_mapped_eset)[!, "cell type:ch1"])
+for cell_type in unique(phenotype_data(ontology_mapped_eset)[!, "ontology_ids"])
     # generate N random genes
     N = rand(15:50)
     genes = sample(feature_names(ontology_mapped_eset), N; replace=false)
     pairings[Symbol(cell_type)] = genes
 end
 
-using OntologyTrees
+GNNCellTypeReferenceGraph.iri_from_id("CL:0000000")
 
 # Create the reference graph
-onto_tree = create_reference_graph(pairings)
+onto_tree = GNNCellTypeReferenceGraph.create_reference_graph(pairings)

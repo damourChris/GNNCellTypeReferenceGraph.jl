4cc7d8904361aa349fe0665216a2eaab861f71c3
Christopher Damour <129872252+damourChris@users.noreply.github.com>
2024-09-22 16:19:13 +0000

    feat: add new step in creation by connecting genes using STRINGdb interactions scores chore: move load_input to utils and add new function for splitting arrays

--- src/main.jl
+++ src/main.jl
@@ -1,27 +1,98 @@
+function connect_genes!(onto_tree::OntologyTree, pairings::Dict{Symbol,Vector{String}})
+    # Get the genes that are in the ontology tree
+    graph = onto_tree.graph
 
-function load_input(eset_file_path::String)
-
-    # We can use the R script to get the cell type marker genes
-    # This is a temporary solution until we have a better way to get the cell type marker genes
-    r_script_path = joinpath(@__DIR__, "get_cell_type_marker_genes.R")
-    eset = load_eset(eset_file_path)
-
-    @rput eset r_script_path
-    R"""
-    source(r_script_path)
-
-    pairings <- get_cell_type_marker_genes(eset)
-    """
-
-    pairs = @rget pairings
-
-    # Convert the pairings to a Julia dictionary
-    ref_eset_pairings = Dict{Symbol,Vector{String}}()
-    for (key, value) in pairings_R
-        ref_eset_pairings[key] = value
+    # We assume that the genes are already mapped to Ensembl IDS
+    # If not exit 
+    # TODO: Conditionally load EnsemblGeneMapping for this
+    if !all([startswith(gene, "ENSG") for gene in genes_to_map])
+        error("Genes are not mapped to Ensembl IDs")
     end
 
-    return ref_eset_pairings
+    genes_to_map = unique(reduce(vcat, values(pairings)))
+
+    @info "Mapping $(length(genes_to_map)) genes to peptides"
+    gene_pep_mapping_result = get_pep_mapping(genes_to_map)
+
+    # We can convert this to a dictionary for faster lookup
+    # This will have unique peptides as keys and a vector of genes as values
+    pep_gene_mapping = Dict{String,Vector{String}}()
+    for (gene, pep) in
+        zip(gene_pep_mapping_result[!, "ensembl_gene_id"],
+            gene_pep_mapping_result[!, "ensembl_peptide_id"])
+        if haskey(pep_gene_mapping, pep)
+            push!(pep_gene_mapping[pep], gene)
+        else
+            pep_gene_mapping[pep] = [gene]
+        end
+    end
+
+    @info "Getting interactions for $(length(keys(pep_gene_mapping))) peptides"
+    interactions_result::DataFrame = get_interactions_batched(collect(keys(pep_gene_mapping)))
+
+    # We can convert this to a dictionary for faster lookup
+    # Keys: Tuple of peptides
+    # Values: Interaction score
+    interactions = Dict{Tuple{String,String},Float64}()
+    for interaction in eachrow(unique(interactions_result))
+        (; stringId_A, stringId_B, score) = interaction
+
+        # The strings will have the format "ORGANISM_ID.PEP_ID"
+        # We only want the PEP_ID
+        pep1 = string(split(stringId_A, ".")[2])
+        pep2 = string(split(stringId_B, ".")[2])
+
+        interactions[(pep1, pep2)] = score
+    end
+
+    # Now that have interactions scores, we need to translate back into interactions strength 
+    # between genes
+    # Keys: Tuple of genes
+    # Values: Interaction score
+    # Need to make sure that we deal with multiple mappings 
+    # -> We can either take the average, max, min, sum or median of the scores
+    # -> Here, we will take the average
+    # We can also add a threshold to filter out weak interactions
+    # For performance, we can check ahead of time what gene-gene interaction have multiple scores and reduce them 
+    # Then we can combine everything
+    gene_interactions = Dict{Tuple{String,String},Float64}()
+    for (pep1, pep2) in keys(interactions)
+        # If the interactions has mappings to genes that are not in the gene list, we can skip them
+        if !haskey(pep_gene_mapping, pep1) || !haskey(pep_gene_mapping, pep2)
+            continue
+        end
+
+        genes1 = pep_gene_mapping[pep1]
+        genes2 = pep_gene_mapping[pep2]
+
+        for gene1 in genes1, gene2 in genes2
+            # Altought we are not considering self-interactions, we should check for them
+            # -> This is because the same gene can have multiple peptides and thus multiple interactions
+            if gene1 == gene2
+                continue
+            end
+
+            key = (gene1, gene2)
+            if haskey(gene_interactions, key)
+                gene_interactions[key] = (gene_interactions[key] +
+                                          interactions[(pep1, pep2)]) /
+                                         2
+            else
+                gene_interactions[key] = interactions[(pep1, pep2)]
+            end
+        end
+    end
+
+    # We can now add the edges with weights to the graph
+    for (gene1, gene2) in keys(gene_interactions)
+        # Find the indinces for both genes
+        gene1_idx = graph[gene1, :id]
+        gene2_idx = graph[gene2, :id]
+        add_edge!(graph, gene1_idx, gene2_idx)
+
+        # Add the weight to the edge
+        set_prop!(graph, gene1_idx, gene2_idx, :weight, gene_interactions[(gene1, gene2)])
+    end
 end
 
 function create_reference_graph(pairings::Dict{Symbol,Vector{String}}; onto="cl",
@@ -47,6 +118,8 @@ function create_reference_graph(pairings::Dict{Symbol,Vector{String}}; onto="cl"
     onto_tree = OntologyTree(base_term, required_terms; max_parent_limit=20)
 
     add_genes!(onto_tree, unique(vcat(values(term_paired_nodes)...)))
+
+    connect_genes!(onto_tree, pairings)
 
     connect_term_genes!(onto_tree, term_paired_nodes)
 
--- src/utils.jl
+++ src/utils.jl
@@ -11,3 +11,206 @@ end
 function iri_from_id(id::Symbol)::String
     return iri_from_id(string(id))
 end
+function load_input(eset_file_path::String)
+
+    # We can use the R script to get the cell type marker genes
+    # This is a temporary solution until we have a better way to get the cell type marker genes
+    r_script_path = joinpath(@__DIR__, "get_cell_type_marker_genes.R")
+    eset = load_eset(eset_file_path)
+
+    @rput eset r_script_path
+    R"""
+    source(r_script_path)
+
+    pairings <- get_cell_type_marker_genes(eset)
+    """
+
+    pairs = @rget pairings
+
+    # Convert the pairings to a Julia dictionary
+    ref_eset_pairings = Dict{Symbol,Vector{String}}()
+    for (key, value) in pairings_R
+        ref_eset_pairings[key] = value
+    end
+
+    return ref_eset_pairings
+end
+
+function split_array(arr, L)
+    K = length(arr)
+
+    if K < L
+        return [arr]
+    end
+
+    n = div(K - 1, L) + 1
+    r = mod(K, L)
+
+    result = Vector{Vector{eltype(arr)}}()
+
+    start_idx = 1
+    for i in 1:(n - 1)
+        push!(result, view(arr, start_idx:(start_idx + L - 1)))
+        start_idx += L
+    end
+
+    if r > 0
+        push!(result, view(arr, start_idx:(start_idx + r - 1)))
+    end
+
+    return result
+end
+
+function get_pep_mapping(ids::Vector{String})
+    df = DataFrame()
+
+    for id_chunk in split_array(ids, 49)
+        result = map_to_stable_ensembl_peptide(id_chunk, "ensembl_gene_id")
+        append!(df, result)
+    end
+
+    return df
+end
+
+function get_interactions_batched(ids::Vector{String})
+    df = DataFrame()
+    chunks = split_array(ids, 50)
+
+    for (index, id_chunk) in enumerate(chunks)
+        println("Processing chunk $index / $(length(chunks))")
+        result = STRINGdb.get_interactions(id_chunk)
+        append!(df, result)
+    end
+
+    return df
+end
+
+function export_to_graphxml(tree::OntologyTree, filename::String)
+    graph = tree.graph
+    xdoc = XMLDocument()
+
+    # Create the root element
+    xroot = create_root(xdoc, "graphml")
+    set_attribute(xroot, "xmlns", "http://graphml.graphdrawing.org/xmlns")
+
+    # For each fieldnames in the Term struct, create a key element
+    for field in fieldnames(Term)
+        xkey = new_child(xroot, "key")
+        set_attribute(xkey, "id", field)
+        set_attribute(xkey, "for", "node")
+        set_attribute(xkey, "attr.name", field)
+        set_attribute(xkey, "attr.type", "string")
+    end
+
+    for field in [:id, :type, :label]
+        xkey = new_child(xroot, "key")
+        set_attribute(xkey, "id", field)
+        set_attribute(xkey, "for", "node")
+        set_attribute(xkey, "attr.name", field)
+        set_attribute(xkey, "attr.type", "string")
+    end
+
+    for field in [:expression, :proportion]
+        xkey = new_child(xroot, "key")
+        set_attribute(xkey, "id", field)
+        set_attribute(xkey, "for", "node")
+        set_attribute(xkey, "attr.name", field)
+        set_attribute(xkey, "attr.type", "float")
+    end
+
+    for field in [:weight]
+        xkey = new_child(xroot, "key")
+        set_attribute(xkey, "id", field)
+        set_attribute(xkey, "for", "edge")
+        set_attribute(xkey, "attr.name", field)
+        set_attribute(xkey, "attr.type", "float")
+    end
+
+    # Define graph attributes
+    xgraph = new_child(xroot, "graph")
+    set_attribute(xgraph, "id", "G")
+    set_attribute(xgraph, "edgedefault", "directed") # Assuming directed graph
+
+    vertice_with_term_prop = [v_index
+                              for (v_index, v_props) in graph.vprops
+                              if v_props[:type] == :term]
+
+    # Add term node metadata
+    for v in vertice_with_term_prop
+        xnode = new_child(xgraph, "node")
+        set_attribute(xnode, "id", string(v))
+
+        term = get_prop(graph, v, :term)
+
+        xdata = new_child(xnode, "data")
+        set_attribute(xdata, "key", :type)
+        add_text(xdata, "cell")
+
+        for field in fieldnames(Term)
+            xdata = new_child(xnode, "data")
+            set_attribute(xdata, "key", field)
+            add_text(xdata, string(getfield(term, field)))
+        end
+
+        try
+            proportion = get_prop(graph, v, :proportion)
+            if ismissing(proportion)
+                continue
+            end
+            xdata = new_child(xnode, "data")
+            set_attribute(xdata, "key", :proportion)
+            add_text(xdata, string(proportion))
+        catch
+            continue
+        end
+    end
+
+    vertice_with_gene_prop = [v_index
+                              for (v_index, v_props) in graph.vprops
+                              if v_props[:type] == :gene]
+
+    # Add term node metadata
+    for v in vertice_with_gene_prop
+        xnode = new_child(xgraph, "node")
+        set_attribute(xnode, "id", string(v))
+
+        id = get_prop(graph, v, :id)
+        xdata = new_child(xnode, "data")
+        set_attribute(xdata, "key", :id)
+        add_text(xdata, string(id))
+
+        gene_expression = get_prop(graph, v, :expression)
+        if (!ismissing(gene_expression))
+            xdata = new_child(xnode, "data")
+            set_attribute(xdata, "key", :expression)
+            add_text(xdata, string(gene_expression))
+        end
+
+        xdata = new_child(xnode, "data")
+        set_attribute(xdata, "key", :label)
+        add_text(xdata, string(id))
+
+        xdata = new_child(xnode, "data")
+        set_attribute(xdata, "key", :type)
+        add_text(xdata, "gene")
+    end
+
+    # Add edges
+    for e in edges(graph)
+        xedge = new_child(xgraph, "edge")
+        set_attribute(xedge, "source", string(src(e)))
+        set_attribute(xedge, "target", string(dst(e)))
+
+        if !has_prop(graph, e, :weight)
+            continue
+        end
+
+        weight = get_prop(graph, e, :weight)
+        xdata = new_child(xedge, "data")
+        set_attribute(xdata, "key", "weight")
+        add_text(xdata, string(weight))
+    end
+
+    # Save XML document to file
+    return save_file(xdoc, filename)
+end

004199b6811f82a88a5c751aad89bc49aa30fd88
Christopher Damour <129872252+damourChris@users.noreply.github.com>
2024-09-22 16:22:39 +0000

    sandbox: update workflow to include adding gene-gene edges

--- sandbox/sandbox.jl
+++ sandbox/sandbox.jl
@@ -1,28 +1,42 @@
 using Pkg
 Pkg.activate(@__DIR__)
 
-using ExpressionData
-using EnsemblGeneMapping
-using OntologyMapping
-using OntologyTrees
-using StatsBase
+begin
+    using ExpressionData
+    using EnsemblGeneMapping
+    using DataFrames
+    using ProteinMapping
+    using LightXML
+    using STRINGdb
+    using OntologyMapping
+    using OntologyTrees
+    using StatsBase
+end
 
 include("../src/GNNCellTypeReferenceGraph.jl")
 
-eset = load_eset(joinpath(@__DIR__, "data", "GSE22886-GPL96_series_matrix.rds"))
-mapped_eset = map_to_ensembl(eset, "entrezgene_id"; gene_col="ENTREZ_GENE_ID")
+ontology_mapped_eset_path = joinpath(@__DIR__, "data",
+                                     "GSE22886-GPL96_series_matrix_ontology.jld2")
 
-replacements = ("cells" => "cell",
-                "Naïve" => "naive",
-                "Monocytes" => "monocyte",
-                "Macrophages" => "macrophage",
-                "Neutrophils" => "neutrophil",
-                "CD8+" => "CD8",
-                "CD4+" => "CD4",
-                "Tregs" => "Treg")
+if !isfile(ontology_mapped_eset_path)
+    eset = load_eset(joinpath(@__DIR__, "data", "GSE22886-GPL96_series_matrix.rds"))
+    mapped_eset = map_to_ensembl(eset, "entrezgene_id"; gene_col="ENTREZ_GENE_ID")
 
-ontology_mapped_eset = map_to_ontology(mapped_eset, "cl"; ontology_col="cell type:ch1",
-                                       replacements=replacements)
+    replacements = ("cells" => "cell",
+                    "Naïve" => "naive",
+                    "Monocytes" => "monocyte",
+                    "Macrophages" => "macrophage",
+                    "Neutrophils" => "neutrophil",
+                    "CD8+" => "CD8",
+                    "CD4+" => "CD4",
+                    "Tregs" => "Treg")
+
+    ontology_mapped_eset = map_to_ontology(mapped_eset, "cl"; ontology_col="cell type:ch1",
+                                           replacements=replacements)
+    save_eset(ontology_mapped_eset, ontology_mapped_eset_path)
+else
+    ontology_mapped_eset = load_eset(ontology_mapped_eset_path)
+end
 
 # Now we need to get the pairings
 # -> This step is to establish the links between genes and cell types
@@ -36,7 +50,9 @@ for cell_type in unique(phenotype_data(ontology_mapped_eset)[!, "ontology_ids"])
     pairings[Symbol(cell_type)] = genes
 end
 
-GNNCellTypeReferenceGraph.iri_from_id("CL:0000000")
-
 # Create the reference graph
 onto_tree = GNNCellTypeReferenceGraph.create_reference_graph(pairings)
+
+GNNCellTypeReferenceGraph.export_to_graphxml(onto_tree,
+                                             joinpath(@__DIR__, "data",
+                                                      "cell_type_reference_graph.graphml"))
